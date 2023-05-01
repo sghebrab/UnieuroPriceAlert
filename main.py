dd508f198e402c9da989c99d49b1845088cc66fd
Gabriele Bellicini <gabriele.bellicini@gmail.com>
2023-05-01 11:38:55 +0200

    Added the possibility to track more than one product.

--- main.py
+++ main.py
@@ -10,10 +10,7 @@ import json
 conf_file = open("conf.json", "r")
 # Then load it into a dictionary
 conf = json.load(conf_file)
-# The price you are willing to pay for the product
-target_price = float(conf["target-price"])
-# URL of the product on the Unieuro website
-url = conf["url"]
+conf_file.close()
 
 # Copied and pasted from the web
 def send_email(subject, body, sender, recipients, password):
@@ -26,42 +23,52 @@ def send_email(subject, body, sender, recipients, password):
     smtp_server.sendmail(sender, recipients, msg.as_string())
     smtp_server.quit()
 
-# Load from last_price.txt the previous price found on the web page
-previous_price_file = open("last_price.txt", "r")
-previous_price = previous_price_file.readline()
-if previous_price == "":
-    previous_price = 1e6
-else:
-    previous_price = float(previous_price)
-previous_price_file.close()
-
-# Load the HTML page with a GET request
-html_page = requests.get(url).text
-# Initialize the BeautifulSoup object
-soup = BeautifulSoup(html_page, "html.parser")
-# Find the right div that has the price in children spans
-div_price_right = soup.find("div", class_="pdp-right__price")
-span_price_int = div_price_right.find("span", class_="integer")
-span_price_dec = div_price_right.find("span", class_="decimal")
-# Get the text from both spans to obtain the price in decimal form
-price_int = span_price_int.get_text()
-price_dec = span_price_dec.get_text().replace(",", ".")
-price_final = float(price_int + price_dec)
-# For each check, write date and time, plus the current retrieved price (for historical purposes)
-log_file = open("connections.log", "a")
-log_file.write(str(datetime.now()) + " --> " + str(price_final) + "€\n")
-log_file.close()
-# If current price is less than the target AND previously it was not
-# (otherwise it would have already notified you), then send an e-mail
-if price_final <= target_price and previous_price > target_price:
-    subject = "Price dropped!"
-    body = "Current price for " + url + " --> " + str(price_final) + "€"
-    sender = conf["sender"]
-    recipients = [conf["sender"]]
-    password = conf["password"]
-    send_email(subject, body, sender, recipients, password)
-
-# Finally, write the current price to the file, so that it can be retrieved later
-previous_price_file = open("last_price.txt", "w")
-previous_price_file.write(str(price_final))
-previous_price_file.close()
+# For each product to track in the JSON file
+for product in conf["products"]:
+    # Load the HTML page with a GET request
+    html_page = requests.get(product["url"]).text
+    # Initialize the BeautifulSoup object
+    soup = BeautifulSoup(html_page, "html.parser")
+    # Find the right div that has the price in children spans
+    div_price_right = soup.find("div", class_="pdp-right__price")
+    span_price_int = div_price_right.find("span", class_="integer")
+    span_price_dec = div_price_right.find("span", class_="decimal")
+    # Get the text from both spans to obtain the price in decimal form
+    price_int = span_price_int.get_text()
+    price_dec = span_price_dec.get_text().replace(",", ".")
+    price_final = float(price_int + price_dec)
+    # Log current time and price for each product, just for information purposes
+    connections_log = open("connections.log", "a")
+    connections_log.write(str(datetime.now()) + " --> " + product["friendly-name"] + " --> " + str(price_final) + "€\n")
+    connections_log.close()
+    # If current price is lower than the threshold AND before this time it was not
+    # (otherwise it would have already notified you), then send an e-mail
+    if price_final <= product["target-price"] and product["latest-price"] > product["target-price"]:
+        subject = "Price under threshold!"
+        body = "Current price for " + product["friendly-name"] + " --> " + str(price_final) + "€\n"
+        body += "Link --> " + product["url"]
+        sender = conf["sender"]
+        recipients = [conf["sender"]]
+        password = conf["password"]
+        send_email(subject, body, sender, recipients, password)
+        # If the current price is also the historical minimum, replace the latter with the newest value
+        if price_final < product["lowest-price"]:
+            product["lowest-price"] = price_final
+    # If current price is not under threshold, but it is the lowest seen, notify the user to let him know
+    # that current price is an historical minimum (maybe he's willing to pay that amount...)
+    elif price_final < product["lowest-price"]:
+        subject = "New historical minimum!"
+        body = "Current price for " + product["friendly-name"] + " --> " + str(price_final) + "€\n"
+        body += "Previous historical minimum --> " + str(product["lowest-price"]) + "€\n"
+        body += "Link --> " + product["url"]
+        sender = conf["sender"]
+        recipients = [conf["sender"]]
+        password = conf["password"]
+        send_email(subject, body, sender, recipients, password)
+        product["lowest-price"] = price_final
+    # Finally, replace the latest price with the newly measured one
+    product["latest-price"] = price_final
+# Dump the dictionary to the conf.json file
+conf_file = open("conf.json", "w")
+json.dump(conf, conf_file, indent=4)
+conf_file.close()
